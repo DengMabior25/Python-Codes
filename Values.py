4868a1df719f9e0dcf8fe6b568ed7dceaac8391a
DengMabior25 <deng.mabior_ti25@nusaputra.ac.id>
2026-02-26 19:15:53 +0700

    forth commit

---
+++ Values.py
@@ -0,0 +1,7 @@
+val1 = float(input("Value 1: "))
+val2 = float(input("Value 2: "))
+val3 = float(input("Value 3: "))
+
+average = (val1 + val2 + val3) / 3 
+
+print(f"The average result is: {average}")
