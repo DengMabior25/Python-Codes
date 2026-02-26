d0f9a8037116c96c2dc62f30422c9b7070833007
DengMabior25 <deng.mabior_ti25@nusaputra.ac.id>
2026-02-26 19:12:19 +0700

    third commit

---
+++ LargestNo.py
@@ -0,0 +1,9 @@
+num1 = float(input("Enter number 1: "))
+num2 = float(input("Enter number 2: "))
+
+if num1 > num2:
+    print("The largest number is: {num1}")
+elif num2 > num1:
+    print("The largest number is: {num2}")
+else:
+    print("Both numbers are equal")
