132ad4fe9fbeefafc1c5089fa990f76a1c98da7a
DengMabior25 <deng.mabior_ti25@nusaputra.ac.id>
2026-02-26 19:08:49 +0700

    second commit

---
+++ DiscountCalculation.py
@@ -0,0 +1,9 @@
+price = float(input("Enter the item price: "))
+
+if price >= 100000:
+    discount = price * 0.10
+    total = price - discount
+else:
+    total = price
+
+print(f"Total payment: {total}")
