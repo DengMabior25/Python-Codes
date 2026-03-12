f1bbcceb456921e73b370501db6e2dd17121642e
DengMabior25 <deng.mabior_ti25@nusaputra.ac.id>
2026-03-12 14:26:05 +0700

    second

---
+++ AreaPerimeter.py
@@ -0,0 +1,8 @@
+length = float(input("Enter the length of rectangle: "))
+width = float(input("Enter the width of rectangle: "))
+
+area = length * width
+perimeter = 2 * (length * width)
+
+print(area)
+print(perimeter)
